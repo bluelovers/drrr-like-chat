44ac964d04697192b567652d410c7bbdf8aedee3
bluelovers <codelovers@users.sourceforge.net>
2011-11-13 20:14:41 +0800

    <?php echo Dura_Class_Icon::getIconUrl($icon) ?>

--- dura/trust_path/template/default.default.php
+++ dura/trust_path/template/default.default.php
@@ -18,7 +18,7 @@
 <?php foreach ( $dura['icons'] as $icon => $file ) : ?>
 <li>
 <label>
-<div class="icon" style="background-image:url('<?php echo DURA_URL.'/css/'.$file ?>')"></div>
+<div class="icon" style="background-image:url('<?php echo Dura_Class_Icon::getIconUrl($icon) ?>')"></div>
 <input type="radio" name="icon" value="<?php echo $icon ?>" />
 </label>
 </li>
