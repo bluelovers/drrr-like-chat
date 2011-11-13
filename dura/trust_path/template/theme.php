987f41d587b339c8fb6620f42e1c3b038ded3a0c
bluelovers <codelovers@users.sourceforge.net>
2011-11-13 09:27:07 +0800

    soundManager

--- dura/trust_path/template/theme.php
+++ dura/trust_path/template/theme.php
@@ -22,6 +22,17 @@ useComet = <?php e(DURA_USE_COMET) ?>;
 <!--script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.sound.js"></script-->
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/SoundManager2/script/soundmanager2-nodebug-jsmin.js"></script>
 
+<script>
+soundManager.url = '<?php e(DURA_URL) ?>/js/SoundManager2/swf/';
+soundManager.onready(function() {
+	messageSound = soundManager.createSound({
+	  id: 'messageSound',
+	  url: '<?php e(DURA_URL) ?>/js/sound.mp3',
+	  volume: 100
+	});
+});
+</script>
+
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.corner.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.chat.js"></script>
 <?php endif ?>
