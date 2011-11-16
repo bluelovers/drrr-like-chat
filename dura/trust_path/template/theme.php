317d299c379be53cd941e6cedde17576db5d851b
bluelovers <codelovers@users.sourceforge.net>
2011-11-16 22:48:13 +0800

    <?php if ( Dura::$controller == 'room' && Dura::$action == 'default' ) : ?>

--- dura/trust_path/template/theme.php
+++ dura/trust_path/template/theme.php
@@ -18,7 +18,7 @@ useComet = <?php e(DURA_USE_COMET) ?>;
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/translator.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/language/<?php e(Dura::$language) ?>.js"></script>
 
-<?php if ( Dura::$controller == 'room' && Dura::$action != 'askpw' ) : ?>
+<?php if ( Dura::$controller == 'room' && Dura::$action == 'default' ) : ?>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.sound.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.corner.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.chat.js"></script>
