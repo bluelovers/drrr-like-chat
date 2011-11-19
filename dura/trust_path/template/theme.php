90edcf13e4e4fff30d49ce760033df72eeebd14e
bluelovers <codelovers@users.sourceforge.net>
2011-11-19 19:51:05 +0800

    language : '<?php echo Dura::user()->getLanguage(); ?>',

--- dura/trust_path/template/theme.php
+++ dura/trust_path/template/theme.php
@@ -1,18 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width = 620" />
 <title><?php e(t(DURA_TITLE)) ?> | <?php e(t(DURA_SUBTITLE)) ?></title>
 <link href="<?php echo DURA_URL; ?>/css/style.css" rel="stylesheet" type="text/css" media="screen" />
-<script type="text/javascript" src="http://www.google.com/jsapi"></script> 
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script type="text/javascript"><!--
-google.load("language", "1"); 
+google.load("language", "1");
 google.load("jquery", "1");
 google.load("jqueryui", "1");
 duraUrl = "<?php e(DURA_URL) ?>";
 GlobalMessageMaxLength = <?php e(DURA_MESSAGE_MAX_LENGTH) ?>;
 useComet = <?php e(DURA_USE_COMET) ?>;
+// bluelovers
+user = {
+	language : '<?php echo Dura::user()->getLanguage(); ?>',
+};
+// bluelovers
 //-->
 </script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/translator.js"></script>
