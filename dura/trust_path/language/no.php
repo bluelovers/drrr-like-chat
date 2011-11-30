7180192465f0453938739d0ab8e8a8ef4e8b036e
bluelovers <codelovers@users.sourceforge.net>
2011-12-01 07:23:10 +0800

    http://drrrchat.com/localize.php#no

---
+++ dura/trust_path/language/no.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @see http://drrrchat.com/localize.php#no
+ * @link http://drrrchat.com/localize.php#no
+ */
+
+return array(
+	"Please input name." => "Skriv inn navn.",
+	"Name should be less than 10 letters." => "Navn må være maksimum 10 bokstaver.",
+	"Member should be more than {1}." => "Brukerkapasiteten må minimum {1}.",
+	"Member should be less than {1}." => "Brukerkapasiteten må være mindre enn {1}.",
+	"Cannot create new room any more." => "Du kan ikke opprette et nytt rom.",
+	"{1} logged in." => "-- {1} logget inn.",
+	"{1} logged out." => "-- {1} logget ut.",
+	"{1} lost the connection." => "-- {1} ble frakoblet.",
+	"Data Error: Room creating failed." => "Feil: Det er ikke mulig å opprette et nytt rom.",
+	"Room Name" => "Romnavn",
+	"Max Members" => "Brukerkapasitet",
+	"{1} members" => "{1} brukere",
+	"Up to {1} rooms can be created." => "*Maksimum {1} rom kan bli opprettet.",
+	"Create Room" => "Opprett rom.",
+	'If auto reload doesn\'t work,  please click <a href="{1}">here</a>.' => 'If the page does not automatically reload, please click <a href="{1}">here</a>',
+	"LOGOUT" => "LOGG UT",
+	"CREATE ROOM" => "OPPRETT ROM",
+	"Lounge" => "LOBBY",
+	"full" => "fullt",
+	"LOGIN" => "LOGG INN",
+	"Login error happened." => "Det oppstod en feil ved pålogging.",
+	"ENTER" => "ENTER",
+	"Room not found." => "Rom ikke funnet.",
+	"Room is full." => "Rommet er fullt.",
+	"{1} users online!" => "{1} brukere online!",
+	"Admin" => "Admin",
+	"Admin ID" => "Admin ID",
+	"Password" => "Passord",
+	"ID or password is wrong." => "ID eller passord er feil.",
+	"Announce" => "Kunngjøring",
+	"Admin Announce" => "Admin Kunngjøring",
+	"{1} is a new host." => "-- {1} er en ny vert.",
+	"Change" => "Bytt.",
+	"Same name user exists. Please rename or change icon." => "Brukernavnet eksistrer allerede. Bytt navn eller ikon.",
+	"You are not host." => "Du er ikke vert.",
+	"Room name is blank." => "Romnavn er tomt.",
+	"Room name is modified." => "Romnavn er endret.",
+	"Handover host" => "Overlevering av vert",
+	"Ban user" => "Steng bruker ute.",
+	"Host is invaild." => "Verten er ugyldig.",
+	"User not found." => "Bruker ikke funnet.",
+	"Gave host rights to {1}." => "Ga vertsrettigheter til {1}.",
+	"User is invaild." => "Bruker er ugyldig.",
+	"Banned {1}." => "Utestengt {1}.",
+	"Language" => "Språk",
+	"The language is not in the option." => "Språket er ikke et alternativ.",
+	"Durarara like chat room" => "Durarara-lignende chat",
+	"Durarara fan community" => "Durarara fan community",
+
+);
+
+?>
