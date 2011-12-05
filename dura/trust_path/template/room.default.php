be8a8cb7a1ff36a13f15af4391fe0da672ba4464
bluelovers <codelovers@users.sourceforge.net>
2011-12-05 20:00:21 +0800

    <?php echo rawurlencode(DURA_URL); ?>

--- dura/trust_path/template/room.default.php
+++ dura/trust_path/template/room.default.php
@@ -18,7 +18,7 @@
 </h2>
 <div id="socialButton">
 	<a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php echo DURA_URL ?>" data-text="<?php e(htmlspecialchars(t("I'm now chatting at room '{1}'!"))) ?>" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-	<iframe src="http://www.facebook.com/plugins/like.php?app_id=154452591321345&amp;href=http%3A%2F%2Fdrrrchat.com&amp;send=false&amp;layout=button_count&amp;width=120&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=trebuchet+ms&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:21px;" allowTransparency="true"></iframe>
+	<iframe src="http://www.facebook.com/plugins/like.php?app_id=154452591321345&amp;href=<?php echo rawurlencode(DURA_URL); ?>&amp;send=false&amp;layout=button_count&amp;width=120&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=trebuchet+ms&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:21px;" allowTransparency="true"></iframe>
 </div>
 <div>
 <textarea name="message"></textarea>
