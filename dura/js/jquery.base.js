37f5bd2d94a11012fbb58cfedad42e90a9ffa214
bluelovers <codelovers@users.sourceforge.net>
2011-12-09 21:13:19 +0800

    $('body').removeClass('nojqmobile');

--- dura/js/jquery.base.js
+++ dura/js/jquery.base.js
@@ -1,41 +1,48 @@
 jQuery(function($) {
-	var fixgeometry = function() {
-		/* Some orientation changes leave the scroll position at something
-		* that isn't 0,0. This is annoying for user experience. */
-		scroll(0, 0);
+	$(document)
+		.one('ready', function() {
+			var fixgeometry = function() {
+				/* Some orientation changes leave the scroll position at something
+				* that isn't 0,0. This is annoying for user experience. */
+				scroll(0, 0);
 
-		/* Calculate the geometry that our content area should take */
-		var header = $('div[data-role="header"]:visible');
-		var footer = $('div[data-role="footer"]:visible');
-		var content = $('div[data-role="content"]:visible');
-		var viewport_height = $(window).height();
+				/* Calculate the geometry that our content area should take */
+				var header = $('div[data-role="header"]:visible');
+				var footer = $('div[data-role="footer"]:visible');
+				var content = $('div[data-role="content"]:visible');
+				var viewport_height = $(window).height();
 
-		var content_height = viewport_height - header.outerHeight() - footer.outerHeight();
+				var content_height = viewport_height - header.outerHeight() - footer.outerHeight();
 
-		/* Trim margin/border/padding height */
-		content_height -= (content.outerHeight() - content.height());
-		content.height(content_height);
-	}; /* fixgeometry */
+				/* Trim margin/border/padding height */
+				content_height -= (content.outerHeight() - content.height());
+				content.height(content_height);
+			}; /* fixgeometry */
 
-	$(document).ready(function() {
-		$(window).bind("resize", fixgeometry);
+			$(window).bind("resize", fixgeometry);
 
-		$(window).bind("orientationchange pageshow", function() {
+			$(window).bind("orientationchange pageshow", function() {
+				$(window).triggerHandler('resize');
+			});
+
+			$('#login')
+				.css({
+					position : 'relative',
+				})
+				.find('> div')
+					.andSelf()
+						.addClass('clearfix')
+			;
+
+			$('#login').center({
+				against : 'parent',
+			});
+
 			$(window).triggerHandler('resize');
-		});
-
-		$('#login').center({
-			against : 'parent',
-		});
-	});
-
-	$('#login')
-		.css({
-			position : 'relative',
 		})
-		.find('> div')
-			.andSelf()
-				.addClass('clearfix')
+		.ready(function(){
+			$('body').removeClass('nojqmobile');
+		})
 	;
 
 	var testinput = document.createElement('input');
