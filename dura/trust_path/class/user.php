815a9c76d14ff0afe301cf4c5bf74afedfb735da
bluelovers <codelovers@users.sourceforge.net>
2011-12-11 11:29:27 +0800

    Revert "$this->name = htmlspecialchars(htmlspecialchars_decode($name));"
    This reverts commit 92c808fbb468616de894a1338b0ef126e4e6b59c.

--- dura/trust_path/class/user.php
+++ dura/trust_path/class/user.php
@@ -38,7 +38,7 @@ class Dura_Class_User
 
 	public function login($name, $icon, $language, $admin = false)
 	{
-		$this->name = htmlspecialchars(htmlspecialchars_decode($name));
+		$this->name = $name;
 		$this->icon = $icon;
 		$this->id = md5($name.getenv('REMOTE_ADDR'));
 		$this->language = $language;
