9c1a9643a87be4135b39122d140568bd4e9eef40
bluelovers <codelovers@users.sourceforge.net>
2012-04-22 11:46:11 +0800

    .

---
+++ dura/trust_path/tpl/theme.copyright.php
@@ -0,0 +1,7 @@
+			<div class="copyright">
+				<a href="#page_admin" data-rel="dialog"><?php e("Admin") ?></a> |
+				Durarara-like-chat Copyright (c) 2010 <a href="http://suin.asia/" target="_blank">Suin</a> | Fork (c) <?php echo gmdate('Y', time()); ?> <a href="http://bluelovers.net/" target="_blank">bluelovers</a>
+				<?php if ( !defined('DURA_HIDE_FORK' ) || !DURA_HIDE_FORK ) : ?>
+				 | <a href="https://github.com/bluelovers/drrr-like-chat" target="_blank">get this chat?</a>
+				<?php endif ?>
+			</div>
---
+++ dura/trust_path/tpl/theme.footer.php
@@ -0,0 +1,10 @@
+		<div data-role="footer" data-position="fixed" data-theme="a">
+			<a href="#" data-icon="arrow-u"  data-iconpos="notext" data-direction="reverse" id="scrolltop"> Top </a>
+
+			<!-- 將此標記放在您想要顯示 +1 按鈕的位置 -->
+			<div class="g-plusone" data-size="small" data-href="<?php e(DURA_URL) ?>">
+			</div>
+
+			<!-- 將此顯示呼叫 (render call) 放在適當位置 -->
+			<script type="text/javascript">gapi.plusone.go();</script>
+		</div>
