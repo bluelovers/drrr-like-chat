161a5a548d1d106fe58bcbd3f7c16641bf9eb40a
bluelovers <codelovers@users.sourceforge.net>
2012-04-22 12:57:08 +0800

    <?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?>

--- dura/trust_path/tpl/default.default.php
+++ dura/trust_path/tpl/default.default.php
@@ -4,7 +4,7 @@
 	<div data-role="page" id="page_default" data-theme="a">
 		<div data-role="header" data-position="fixed">
 			<a href="#page_default" data-icon="home"  data-iconpos="notext" data-direction="reverse"> Home </a>
-			<h3> Durarara 聊天室 </h3>
+			<h3><?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?></h3>
 			<a href="#page_default" data-icon="back" data-iconpos="notext"> Logout </a>
 		</div>
 		<div data-role="content">
