d5a550cb60e56369589b7d54c2138ee03489c643
bluelovers <codelovers@users.sourceforge.net>
2012-04-23 04:06:02 +0800

    .

--- dura/trust_path/tpl/trans.php
+++ dura/trust_path/tpl/trans.php
@@ -3,12 +3,12 @@
 
 	<?php $this->set('html.header.script', '<meta http-equiv="refresh" content="10;URL=' . $this->get('url') . '">') ?>
 
-	<div data-role="page" data-theme="a" data-rel="dialog">
+	<div data-role="dialog" data-theme="a" data-rel="dialog" data-ajax="false">
 		<div data-role="header" data-theme="e">
 			<h1><?php e(t("EXIT")) ?></h1>
 		</div>
 		<div data-role="content">
 			<p><?php e($dura['message']) ?></p>
-			<p><?php e(t('If auto reload doesn\'t work,  please click <a href="{1}">here</a>.', $this->get('url'))) ?></p>
+			<p><?php e(t('If auto reload doesn\'t work,  please click <a href="{1}" data-ajax="false">here</a>.', $this->get('url'))) ?></p>
 		</div>
 	</div>
