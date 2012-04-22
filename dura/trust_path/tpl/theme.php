6365bac87bc90505b3055d2d495b4ce62613ab4f
bluelovers <codelovers@users.sourceforge.net>
2012-04-22 09:02:00 +0800

    .

---
+++ dura/trust_path/tpl/theme.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html data-theme="a">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<title><?php echo $this->get('title') ?></title>
+	<link rel="stylesheet" href="<?php e(DURA_URL) ?>/jquery-mobile/themes/dura.min.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile.structure-1.1.0.min.css" />
+	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+	<script type="text/javascript">google.load("jquery", "1");</script>
+	<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
+	<?php echo $this->get('html.header') ?>
+</head>
+<body data-theme="a">
+	<?php echo $this->get('body') ?>
+</body>
+	<?php echo $this->get('html.footer') ?>
+</html>
