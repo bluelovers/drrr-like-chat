e182af552828fb7a4d5fcc26cbb3ced0a2fbb1d5
bluelovers <codelovers@users.sourceforge.net>
2012-04-23 18:42:19 +0800

    .

--- dura/trust_path/tpl/theme.footer.php
+++ dura/trust_path/tpl/theme.footer.php
@@ -1,5 +1,15 @@
 		<div data-role="footer" data-position="fixed" data-theme="a">
-			<a href="#" data-icon="arrow-u"  data-iconpos="notext" data-direction="reverse" id="scrolltop"> Top </a>
+
+
+			<div data-role="controlgroup" data-type="horizontal" class="ui-btn" >
+
+				<a href="#" data-icon="arrow-u"  data-iconpos="notext" data-direction="reverse" id="scrolltop" data-role="button"> Top </a>
+
+				<?php e($this->get('tpl.page.footer.btn.left')); ?>
+
+			</div>
+
+			<?php e($this->get('tpl.page.footer.btn.center')); ?>
 
 			<!-- 將此標記放在您想要顯示 +1 按鈕的位置 -->
 			<div class="g-plusone" data-size="small" data-href="<?php e(DURA_URL) ?>">
@@ -7,4 +17,11 @@
 
 			<!-- 將此顯示呼叫 (render call) 放在適當位置 -->
 			<script type="text/javascript">gapi.plusone.go();</script>
+
+			<div data-role="controlgroup" data-type="horizontal" class="ui-btn-right" >
+
+				<?php e($this->get('tpl.page.footer.btn.right')); ?>
+
+			</div>
+
 		</div>
--- dura/trust_path/tpl/theme.page.header.php
+++ dura/trust_path/tpl/theme.page.header.php
@@ -1,7 +1,21 @@
 		<div data-role="header" data-position="fixed">
-			<a href="<?php echo Dura::url(); ?>" data-icon="home"  data-iconpos="notext" data-direction="reverse" data-inline="true"> Home </a>
+
+			<div data-role="controlgroup" data-type="horizontal" class="ui-btn-left" >
+
+				<?php if ($this->get('tpl.page.header.home', true)): ?>
+					<a href="<?php echo Dura::url(); ?>" data-icon="home"  data-iconpos="notext" data-direction="reverse" data-role="button"> Home </a>
+				<?php endif ?>
+
+				<?php e($this->get('tpl.page.header.btn.left')); ?>
+
+			</div>
+
 			<h3><?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?></h3>
+
 			<div data-role="controlgroup" data-type="horizontal" class="ui-btn-right" >
+
+				<?php e($this->get('tpl.page.header.btn.right')); ?>
+
 				<?php if (Dura::$controller != 'page' && Dura::$action != 'about'): ?>
 					<a href="<?php echo Dura::url('page', 'about'); ?>" data-icon="info" data-iconpos="notext" data-role="button" data-ajax="false"><?php e(t("INFO")) ?></a>
 				<?php endif ?>
@@ -9,4 +23,5 @@
 					<a href="<?php e($this->get('tpl.page.header.back.url', Dura::url(Dura::$controller, Dura::$action).'#page_logout')); ?>" data-icon="back" data-iconpos="notext" data-rel="dialog" data-role="button"><?php e(t("EXIT")) ?></a>
 				<?php endif ?>
 			</div>
+
 		</div>
