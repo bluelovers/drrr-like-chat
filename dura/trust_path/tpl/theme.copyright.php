6b6494516cb573960e43df9ef7b064bb07eeb66e
bluelovers <codelovers@users.sourceforge.net>
2012-04-23 18:47:20 +0800

    .

--- dura/trust_path/tpl/theme.copyright.php
+++ dura/trust_path/tpl/theme.copyright.php
@@ -1,7 +1,11 @@
 			<div class="copyright">
-				<a href="#page_admin" data-rel="dialog"><?php e("Admin") ?></a> |
-				Durarara-like-chat Copyright (c) 2010 <a href="http://suin.asia/" target="_blank">Suin</a> | Fork (c) <?php echo gmdate('Y', time()); ?> <a href="http://bluelovers.net/" target="_blank">bluelovers</a>
+
+				<?php if (Dura::$controller == 'default'): ?>
+					<a href="#page_admin" data-rel="dialog"><?php e("Admin") ?></a> |
+				<?php endif ?>
+
+				Durarara-like-chat Copyright (c) 2010 <a href="http://suin.asia/" target="_blank" rel="external">Suin</a> | Fork (c) <?php echo gmdate('Y', time()); ?> <a href="http://bluelovers.net/" target="_blank" rel="external">bluelovers</a>
 				<?php if ( !defined('DURA_HIDE_FORK' ) || !DURA_HIDE_FORK ) : ?>
-				 | <a href="https://github.com/bluelovers/drrr-like-chat" target="_blank">get this chat?</a>
+				 | <a href="https://github.com/bluelovers/drrr-like-chat" target="_blank" rel="external">get this chat?</a>
 				<?php endif ?>
 			</div>
