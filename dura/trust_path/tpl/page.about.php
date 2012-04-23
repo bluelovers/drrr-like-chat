68ec7df939622754ba04e8923cbe622584a85e8d
bluelovers <codelovers@users.sourceforge.net>
2012-04-23 17:04:37 +0800

    .

---
+++ dura/trust_path/tpl/page.about.php
@@ -0,0 +1,177 @@
+<style>
+.textRight {
+	text-align: right;
+}
+div.header div.content {
+	margin: 0 30px;
+}
+div.header h2 {
+	text-align: left;
+	margin: 10px 0;
+	color: #222;
+	text-shadow: 0px 2px 3px #aaa;
+	font-size: 26px;
+}
+div.header h2 img {
+	height: 28px;
+	widows: 28px;
+	margin: 0 6px 0 0;
+	vertical-align: middle;
+}
+div.header h2.tanaka {
+	color: #3fa2fb;
+	text-shadow: 0px 2px 3px #bfd8ef;
+}
+div.header h2.kanra {
+	color: #fba205;
+	text-shadow: 0px 2px 3px #fbd085;
+}
+div.header h2.zawa {
+	color: #317d36;
+	text-shadow: 0px 2px 3px #accfae;
+}
+div.header h2.gg {
+	color: #eb4b9f;
+	text-shadow: 0px 2px 3px #ebafcf;
+}
+div.header p {
+	line-height: 170%;
+	text-shadow: 0px 2px 3px #999;
+	text-indent: 1em;
+	font-size: 18px;
+}
+#dark {
+	background-color:#333;
+	border:1px solid #000;
+	padding:10px;
+	margin-top:20px;
+}
+#light {
+	background-color:#FFF;
+	border:1px solid #dedede;
+	padding:10px;
+	margin-top:20px;
+}
+li {
+	list-style:none;
+	padding-top:10px;
+	padding-bottom:10px;
+}
+.button, .button:visited {
+	background: #222 url(http://lh3.googleusercontent.com/-n3HkL6yWN34/TlUAvrTXACI/AAAAAAAABh8/Xsd6_mcqZGs/s800/overlay.png) repeat-x;
+	display: inline-block;
+	padding: 5px 10px 6px;
+	color: #fff;
+	text-decoration: none;
+	-moz-border-radius: 6px;
+	-webkit-border-radius: 6px;
+	-moz-box-shadow: 0 1px 3px rgba(0,0,0,0.6);
+	-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.6);
+	text-shadow: 0 -1px 1px rgba(0,0,0,0.25);
+	border-bottom: 1px solid rgba(0,0,0,0.25);
+	position: relative;
+	cursor: pointer
+}
+.button:hover {
+	background-color: #111;
+	color: #fff;
+}
+.button:active {
+	top: 1px;
+}
+.small.button, .small.button:visited {
+	font-size: 11px
+}
+.button, .button:visited,  .medium.button, .medium.button:visited {
+	font-size: 13px;
+	font-weight: bold;
+	line-height: 1;
+	text-shadow: 0 -1px 1px rgba(0,0,0,0.25);
+}
+.large.button, .large.button:visited {
+	font-size: 14px;
+	padding: 8px 14px 9px;
+}
+.super.button, .super.button:visited {
+	font-size: 29px;
+	padding: 8px 14px 9px;
+}
+.pink.button, .magenta.button:visited {
+	background-color: #e22092;
+}
+.pink.button:hover {
+	background-color: #c81e82;
+}
+.green.button, .green.button:visited {
+	background-color: #91bd09;
+}
+.green.button:hover {
+	background-color: #749a02;
+}
+.red.button, .red.button:visited {
+	background-color: #e62727;
+}
+.red.button:hover {
+	background-color: #cf2525;
+}
+.orange.button, .orange.button:visited {
+	background-color: #ff5c00;
+}
+.orange.button:hover {
+	background-color: #d45500;
+}
+.blue.button, .blue.button:visited {
+	background-color: #2981e4;
+}
+.blue.button:hover {
+	background-color: #2575cf;
+}
+.yellow.button, .yellow.button:visited {
+	background-color: #ffb515;
+}
+.yellow.button:hover {
+	background-color: #fc9200;
+}
+
+.pink.button {
+	color: #FFFFFF;
+}
+</style>
+
+	<?php if (!$_SERVER['HTTP_X_REQUESTED_WITH']) $this->extend('theme'); ?>
+
+	<div data-role="page" id="page_default" data-theme="c">
+		<?php e($this->slot('theme.page.header'));?>
+		<div data-role="content">
+			<div class="top">
+				<div class="topContent">
+					<h1>デュラララ!!チャットとは？<span class="subtitle">| みんなが楽しめるチャット</span></h1>
+				</div>
+			</div>
+			<div class="clear">
+			</div>
+			<div class="header">
+				<div class="content">
+					<h2 class="tanaka"><img src="<?php echo DURA_URL;?>/css/icon/tanaka.png" />アニメ『デュラララ!!』に登場するチャットを再現！</h2>
+					<p>デュラララ!!チャット、通称デュラちゃは、アニメ『デュラララ！！』の作中に出てくるチャットを再現したサイトです。チャットを発表した当初から大きな反響があり、今でも多くの方が遊びに来ています。このチャット制作段階の動画はニコニコ動画からご覧になれます^^</p>
+					<div style="text-align:center; margin: 20px 0;">
+						<p><strong>デュラララ！に登場するチャットを再現してみた</strong></p>
+						<script type="text/javascript" src="http://ext.nicovideo.jp/thumb_watch/sm10152002"></script>
+						<noscript>
+						<a href="http://www.nicovideo.jp/watch/sm10152002">【ニコニコ動画】デュラララ！に登場するチャットを再現してみた</a>
+						</noscript>
+					</div>
+					<h2 class="kanra"><img src="<?php echo DURA_URL;?>/css/icon/kanra.png" />誰でも参加することができます！</h2>
+					<p>このチャットは、デュラララ！！が好きな人、デュラララ！！の世界観を楽しみたい人はもちろん、作品自体は見たいことがないんだけど、面白そうだから！と興味をもってくれた人、みんながチャットに加わることができます。もちろん、<strong>無料</strong>で楽しむことができます！</p>
+					<h2 class="zawa"><img src="<?php echo DURA_URL;?>/css/icon/zawa.png" />参加は名前を決めてログインするだけ！</h2>
+					<p>デュラララ！！チャットでは、アカウント登録の必要はありません！チャットに参加するには、自分のハンドルネームを入力してログインするだけです。ログインするとチャットルーム一覧が現れます。そこから自分が、参加したいなぁと思う部屋を選んで入室してみてください^-^)b</p>
+					<p style="text-align:center; margin: 20px 0;"><img src="http://lh5.googleusercontent.com/-WE7pIbnMzqo/TlT8I9O5m5I/AAAAAAAABho/HGhMg2f-6jY/s640/Colors.jpg" /></p>
+					<h2 class="gg"><img src="<?php echo DURA_URL;?>/css/icon/gg.png" />ゆっくり楽しんでいってね</h2>
+					<p>それでは、デュラララ！！チャットの世界をゆっくり楽しんで行ってください〜！</p>
+					<p style="text-align:center; margin: 30px 0;"><a href="<?php echo DURA_URL;?>" class="super button pink">参加はこちらから</a></p>
+				</div>
+			</div>
+		</div>
+		<?php e($this->slot('theme.footer'));?>
+	</div>
+
