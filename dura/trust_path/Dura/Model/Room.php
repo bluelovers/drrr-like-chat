910c1019ddcc4f6aa4c7016734347c65e22aca51
bluelovers <codelovers@users.sourceforge.net>
2012-04-24 22:33:24 +0800

    Dura_Model_Room

--- dura/trust_path/Dura/Model/Room.php
+++ dura/trust_path/Dura/Model/Room.php
@@ -8,6 +8,75 @@
 class Dura_Model_Room
 {
 
+	protected $id = null;
+
+	/**
+	 * @var Dura_Model_Room_XmlHandler
+	 */
+	protected $roomHandler = null;
+
+	/**
+	 * @var Dura_Model_Room_Xml
+	 */
+	protected $roomModels = null;
+
+	/**
+	 * @return Dura_Model_Room
+	 */
+	public static function fromSession($id = null)
+	{
+		if (Dura_Model_Room_Session::isCreated())
+		{
+			$id = Dura_Model_Room_Session::get('id');
+		}
+
+		return self::fromId($id);
+	}
+
+	/**
+	 * @return Dura_Model_Room
+	 */
+	public static function fromId($id)
+	{
+		return new self($id);
+	}
+
+	function __construct($id)
+	{
+		$this->roomHandler = new Dura_Model_Room_XmlHandler;
+
+		if ($id) $this->load($id);
+	}
+
+	function load($id)
+	{
+		$this->roomModel = $this->roomHandler->load($id);
+
+		return $this;
+	}
+
+	/**
+	 * check room exists
+	 *
+	 * @return bool
+	 */
+	function exists()
+	{
+		if ($this->roomModel)
+		{
+			return true;
+		}
+	}
+
+	/**
+	 * @return Dura_Model_Room
+	 */
+	function sessionDelete()
+	{
+		Dura_Model_Room_Session::delete();
+
+		return $this;
+	}
 }
 
 ?>
