3a062fe56f6c9e6d34095a1ba7e797d124e9f189
bluelovers <codelovers@users.sourceforge.net>
2012-04-25 17:15:30 +0800

    class Dura_Model_Lang_List extends Dura_Class_Array

---
+++ dura/trust_path/Dura/Model/Lang/List.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class Dura_Model_Lang_List extends Dura_Class_Array
+{
+	protected $_data_array_ = array(
+		'en-US' => "English",
+		'ja-JP' => "日本語",
+		'ko-KR' => "한국어",
+		'zh-TW' => "中文(繁體)",
+		'ru-RU' => "Русский",
+		'it-IT' => "Italiano",
+		'pl-PL' => "Polska",
+		'zh-CN' => "中文(簡体)",
+		'pt-BR' => "Português(Brazil)",
+		'eo' => "Esperanto",
+		'es-ES' => "Español",
+		'id-ID' => "Bahasa Indonesia",
+		'de-DE' => "Deutsch",
+		'tr-TR' => "Türkçe",
+		'he-IL' => "עברית‎",
+		);
+}
