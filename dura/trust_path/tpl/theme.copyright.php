c5bb0712bb0e2b91c6109694babd82daf26d21ea
bluelovers <codelovers@users.sourceforge.net>
2012-04-26 04:02:18 +0800

    120x120

--- dura/trust_path/tpl/theme.copyright.php
+++ dura/trust_path/tpl/theme.copyright.php
@@ -15,7 +15,7 @@
 			<div class="copyright" data-theme="a" data-role="fieldcontain">
 
 				<p>
-					<img src="http://chart.apis.google.com/chart?cht=qr&chs=100x100&chl=<?php e(urlencode($this->get('tpl.header.canonical', DURA_URL))); ?>&chld=H|0" alt="<?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?>"/>
+					<img src="http://chart.apis.google.com/chart?cht=qr&chs=120x120&chl=<?php e(urlencode($this->get('tpl.header.canonical', DURA_URL))); ?>&chld=H|0" alt="<?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?>"/>
 				</p>
 
 				<p>
