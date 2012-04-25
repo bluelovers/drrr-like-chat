f16492aa1d0cf20feebeb7e2b893ee613fa8786b
bluelovers <codelovers@users.sourceforge.net>
2012-04-25 15:06:08 +0800

    copy

---
+++ dura/trust_path/Dura/Class/Icon.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * A simple description for this script
+ *
+ * PHP Version 5.2.0 or Upper version
+ *
+ * @package    Dura
+ * @author     Hidehito NOZAWA aka Suin <http://suin.asia>
+ * @copyright  2010 Hidehito NOZAWA
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3
+ *
+ */
+
+class Dura_Class_Icon
+{
+
+	// bluelovers
+	static $iconDir = '/css/icon';
+	static $iconPrefix = '';
+	static $iconExt = 'png';
+	// bluelovers
+
+	public static function &getIcons()
+	{
+		static $icons = null;
+
+		if ( $icons === null )
+		{
+			$icons = array();
+			$iconDir = DURA_PATH.Dura_Class_Icon::$iconDir;
+
+			if ( $dir = opendir($iconDir) )
+			{
+				while ( ($file = readdir($dir)) !== false )
+				{
+					if ( preg_match('/^'.Dura_Class_Icon::$iconPrefix.'(.+)\.'.Dura_Class_Icon::$iconExt.'$/', $file, $match) )
+					{
+						list($dummy, $icon) = $match;
+						$icons[$icon] = $file;
+					}
+				}
+
+				closedir($dir);
+			}
+		}
+
+		return $icons;
+	}
+
+	// bluelovers
+	public static function getIconColor($icon) {
+		static $_map;
+
+		if (!isset($_map)) {
+			@include DURA_TRUST_PATH.'/resource/colors.php';
+
+			$_map = array();
+
+			$_map['icon_color'] = (array)$_icon_color;
+		}
+
+		if ((string)$icon) {
+			$color = empty($_map['icon_color'][(string)$icon]) ? 'gray' : $_map['icon_color'][(string)$icon];
+		} else {
+			return false;
+		}
+
+		return $color;
+	}
+	// bluelovers
+
+	public static function getIconUrl($icon)
+	{
+		$url = DURA_URL.Dura_Class_Icon::$iconDir.'/'.Dura_Class_Icon::$iconPrefix.$icon.'.'.Dura_Class_Icon::$iconExt;
+		return $url;
+	}
+}
+
---
+++ dura/trust_path/Dura/Class/User.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * A simple description for this script
+ *
+ * PHP Version 5.2.0 or Upper version
+ *
+ * @package    Dura
+ * @author     Hidehito NOZAWA aka Suin <http://suin.asia>
+ * @copyright  2010 Hidehito NOZAWA
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3
+ *
+ */
+
+class Dura_Class_User
+{
+	protected $name = null;
+	protected $icon = null;
+	protected $id   = null;
+	protected $expire = null;
+	protected $admin = false;
+	protected $language = null;
+
+	protected function __construct()
+	{
+	}
+
+	public static function &getInstance()
+	{
+		static $instance = null;
+
+		if ( $instance === null )
+		{
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	public function login($name, $icon, $language, $admin = false, $password_room = null)
+	{
+		/*
+		$this->name = $name;
+		*/
+		// bluelovers
+		$this->_setName($name, false);
+		// bluelovers
+		$this->icon = $icon;
+		$this->id = md5($name.getenv('REMOTE_ADDR'));
+		$this->language = $language;
+		$this->admin = $admin;
+
+		// bluelovers
+		if (
+			isset($password_room)
+			&& $password_room !== null
+		) {
+			$password_room = empty($password_room) ? 0 : (string) $password_room;
+
+			$this->password_room = $password_room;
+		}
+		// bluelovers
+
+		$_SESSION['user'] = $this;
+	}
+
+	public function loadSession()
+	{
+		if ( isset($_SESSION['user']) and $_SESSION['user'] instanceof self )
+		{
+			$user = $_SESSION['user'];
+			/*
+			$this->name   = $user->name;
+			*/
+			// bluelovers
+			$this->_setName($user->name, true);
+			// bluelovers
+
+			$this->icon   = $user->icon;
+			$this->id     = $user->id;
+			$this->expire = $user->expire;
+			$this->id     = $user->id;
+			$this->language = $user->language;
+			$this->admin  = $user->admin;
+
+			// bluelovers
+			$this->password_room = $user->password_room;
+			// bluelovers
+		}
+	}
+
+	// bluelovers
+	protected function _setName($name, $update_session = false) {
+		$_name = htmlspecialchars(trim(htmlspecialchars_decode((string)$name)));
+		$this->name = $_name;
+
+		if ($update_session) {
+			$_SESSION['user']->name = $_name;
+		}
+	}
+	// bluelovers
+
+	public function isUser()
+	{
+		return ( $this->id !== null );
+	}
+
+	public function isAdmin()
+	{
+		if ( $this->isUser() )
+		{
+			return $this->admin;
+		}
+
+		return false;
+	}
+
+	public function getName()
+	{
+		if ( !$this->isUser() ) return false;
+
+		return $this->name;
+	}
+
+	public function getIcon()
+	{
+		if ( !$this->isUser() ) return false;
+
+		return $this->icon;
+	}
+
+	public function getId()
+	{
+		if ( !$this->isUser() ) return false;
+
+		return $this->id;
+	}
+
+	public function getLanguage()
+	{
+		return $this->language;
+	}
+
+	// bluelovers
+	public function getColor() {
+
+		if ( !$this->isUser() ) return false;
+
+		if (!isset($this->color)) {
+			$this->_handler($this);
+		}
+
+		return $this->color;
+	}
+
+	public function &_handler(&$user) {
+		$icon = $user->getIcon();
+
+		if ($icon && empty($user->color)) {
+			$user->color = Dura_Class_Icon::getIconColor($user->icon);
+		}
+
+		return $user;
+	}
+	// bluelovers
+
+	public function getExpire()
+	{
+		if ( !$this->isUser() ) return false;
+
+		return $this->expire;
+	}
+
+	public function updateExpire()
+	{
+		$this->expire = time() + DURA_TIMEOUT;
+
+		if ( isset($_SESSION['user']) and $_SESSION['user'] instanceof self )
+		{
+			$_SESSION['user']->expire = $this->expire;
+		}
+	}
+
+	// bluelovers
+	public function getPasswordRoom() {
+
+		if ( !$this->isUser() ) return false;
+
+		return $this->password_room;
+	}
+
+	public function setPasswordRoom($password = 0) {
+
+		if ( !$this->isUser() ) return false;
+
+		$password = empty($password) ? 0 : (string)$password;
+		$this->password_room = $password;
+
+		if ( isset($_SESSION['user']) and $_SESSION['user'] instanceof self )
+		{
+			$_SESSION['user']->password_room = $password;
+		}
+	}
+	// bluelovers
+
+}
+
