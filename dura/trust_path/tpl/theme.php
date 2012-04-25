df4cfe4ca53921dccf4d6c02dab39766fbf020e6
bluelovers <codelovers@users.sourceforge.net>
2012-04-26 04:00:33 +0800

    lang="<?php e(Dura::user()->getLanguage() ? Dura::user()->getLanguage() : DURA_LANGUAGE); ?>"

--- dura/trust_path/tpl/theme.php
+++ dura/trust_path/tpl/theme.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html data-theme="a">
+<html data-theme="a" lang="<?php e(Dura::user()->getLanguage() ? Dura::user()->getLanguage() : DURA_LANGUAGE); ?>">
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -8,8 +8,10 @@
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
 	<link rel="canonical" href="<?php e($this->get('tpl.header.canonical', DURA_URL)); ?>"/>
+	<meta http-equiv="Content-Language" content="<?php e(Dura::user()->getLanguage() ? Dura::user()->getLanguage() : DURA_LANGUAGE); ?>" />
 
 	<title><?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?></title>
+
 	<link rel="stylesheet" href="<?php e(DURA_URL) ?>/_temp/jquery-mobile/themes/dura.min.css" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile.structure-1.1.0.min.css" />
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
