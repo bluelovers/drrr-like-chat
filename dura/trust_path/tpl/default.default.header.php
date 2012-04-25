1f417ac06238838d8a7f53141619cc32732de6f0
bluelovers <codelovers@users.sourceforge.net>
2012-04-25 22:15:31 +0800

    .

--- dura/trust_path/tpl/default.default.header.php
+++ dura/trust_path/tpl/default.default.header.php
@@ -5,28 +5,65 @@
 	$(document)
 		.bind("ready, pageinit, pageshow", function(){
 
-			$('#page_default #icons .ui-btn .icon')
-				.hover(function(){
+			$('#page_default #icons .ui-btn')
+				.live('click', function(){
 					var _this = $(this);
 
-					_this
-						.animate({
-							opacity : 1
-						})
-					;
-
-				}, function(){
-					var _this = $(this);
 
 					_this
-						.animate({
-							opacity : 0.25
-						})
+						.parents('#icons')
+							.find('.ui-btn-active .icon')
+								.dequeue()
+								.animate({
+									opacity : 1
+								})
+							.end()
+							.find(':not(.ui-btn-active) .icon')
+								.animate({
+									opacity : 0.25
+								})
 					;
 				})
+				.find('.icon')
+					.hover(function(){
+						var _this = $(this);
+
+						_this
+							.animate({
+								opacity : 1
+							})
+						;
+
+					}, function(){
+						var _this = $(this);
+
+						if (_this.parents('.ui-btn-active').size())
+						{
+							_this
+								.animate({
+									opacity : 1
+								})
+							;
+						}
+						else
+						{
+							_this
+								.animate({
+									opacity : 0.25
+								})
+							;
+						}
+					})
+					.css({
+						opacity : 0.25
+					})
+				.end()
 				.css({
-					opacity : 0.25
+					opacity : 0.8
 				})
+				.parent()
+					.find(':radio')
+						.hide()
 			;
 
 		})
