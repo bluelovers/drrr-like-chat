60ed32628ae725a186da95430c8fd9d30b2d5521
bluelovers <codelovers@users.sourceforge.net>
2012-04-26 13:25:46 +0800

    .

--- dura/trust_path/tpl/theme.css.php
+++ dura/trust_path/tpl/theme.css.php
@@ -1,366 +1,498 @@
-
 #page_lounge .main-box.content-primary
 {
-	padding: 5px;
+	padding:5px;
 }
 
-.content-secondary {
-	text-align: center;
-	padding: 15px;
-}
-
-#page_room .content-secondary.content-float-left, #page_room .content-secondary.content-float-right
+.content-secondary
 {
-	position: absolute;
+	text-align:center;
+	padding:15px;
+}
+
+#page_room .content-secondary.content-float-left,#page_room .content-secondary.content-float-right
+{
+	position:absolute;
 }
 
 #page_room .content-secondary.content-float-right
 {
-	right: 0px;
-	left: auto;
+	right:0;
+	left:auto;
 }
 
 #page_room .content-secondary.content-float-left
 {
-	left: 0px;
-	right: auto;
+	left:0;
+	right:auto;
 }
 
 .content-float-right
 {
-	float: right;
+	float:right;
 }
 
 .content-float-left
 {
-	float: left;
+	float:left;
 }
 
-.dura-ishost.ui-btn, .talk.dura-ishost .avatar, #message .dura-ishost {
-	color: #CD31C1;
+.dura-ishost.ui-btn,.talk.dura-ishost .avatar,#message .dura-ishost
+{
+	color:#CD31C1;
 }
 
 .talk.dura-ishost .avatar
 {
-	text-shadow: 0 1px 0 #94248C;
+	text-shadow:0 1px 0 #94248C;
 }
 
-.room_members .ui-btn h4 {
-    padding-left: 0.5em;
-    padding-right: 0.5em;
+.room_members .ui-btn h4
+{
+	padding-left:.5em;
+	padding-right:.5em;
 }
 
-.room_members .ui-btn h4, .room_members .ui-btn .icon {
-	vertical-align: middle;
+.room_members .ui-btn h4,.room_members .ui-btn .icon
+{
+	vertical-align:middle;
 }
 
-.room_members .ui-btn-inner{
-	padding: 0px;
+.room_members .ui-btn-inner
+{
+	padding:0;
 }
 
 /* Hides from IE-mac \*/
-
-.clearfix {display: inline-block;}
-
-* html .clearfix {height: 1%;}
-.clearfix {display: block;}
-
-.clearfix:after {
-	content: ".";
-	display: block;
-	clear: both;
-	visibility: hidden;
-	line-height: 0;
-	height: 0;
+.clearfix
+{
+	display:inline-block;
 }
 
-.clearfix {
-	display: inline-block;
+* html .clearfix
+{
+	height:1%;
 }
 
-html[xmlns] .clearfix {
-	display: block;
+.clearfix
+{
+	display:block;
 }
 
-* html .clearfix {
-	height: 1%;
+.clearfix:after
+{
+	content:".";
+	display:block;
+	clear:both;
+	visibility:hidden;
+	line-height:0;
+	height:0;
+}
+
+.clearfix
+{
+	display:inline-block;
+}
+
+html[xmlns] .clearfix
+{
+	display:block;
+}
+
+* html .clearfix
+{
+	height:1%;
 }
 
 /* End hide from IE-mac */
-
-.dura-error {
-	border-width: 1px !important;
-	margin-bottom: 1em;
+.dura-error
+{
+	border-width:1px!important;
+	margin-bottom:1em;
 }
 
-.dura-debug {
-	border-width: 1px !important;
-	margin: 1em;
-	padding: 0.5em;
-	font-size: 10px;
+.dura-debug
+{
+	border-width:1px!important;
+	margin:1em;
+	padding:.5em;
+	font-size:10px;
 }
 
-.main-box {
-	margin: auto;
-	max-width: 620px;
-	padding: 15px;
+.main-box
+{
+	margin:auto;
+	max-width:620px;
+	padding:15px;
 }
-#page_default .ui-content form > *, #page_default .ui-content > * {
-	margin-left: auto;
-	margin-right: auto;
-	text-align: center;
+
+#page_default .ui-content form > *,#page_default .ui-content > *
+{
+	margin-left:auto;
+	margin-right:auto;
+	text-align:center;
 }
-#page_default .ui-content form fieldset {
-	margin-left: auto;
-	margin-right: auto;
-	max-width: 300px;
+
+#page_default .ui-content form fieldset
+{
+	margin-left:auto;
+	margin-right:auto;
+	max-width:300px;
 }
-#icons {
-	margin-left: auto;
-	margin-right: auto;
-	max-width: 288px !important;
+
+#icons
+{
+	margin-left:auto;
+	margin-right:auto;
+	max-width:288px!important;
 }
-#icons .ui-btn-inner {
-	padding: 1px;
+
+#icons .ui-btn-inner
+{
+	padding:1px;
 }
-#icons .icon {
-	vertical-align: middle;
+
+#icons .icon
+{
+	vertical-align:middle;
 }
-#icons .ui-radio {
-	margin: 5px;
+
+#icons .ui-radio
+{
+	margin:5px;
 }
-.copyright {
-	/*
+
+.copyright
+{
+/*
 	display: inline;
 	display: inline-block;
 	*/
-	    clear: both;
-	color: #8A8A8A;
-	font-size: 10px;
-	margin: 10px 0;
-	text-align: center;
+	clear:both;
+	color:#8A8A8A;
+	font-size:10px;
+	margin:10px 0;
+	text-align:center;
 }
-#profile div.ui-block-a {
-	width: 70%;
-	font-weight: normal;
+
+#profile div.ui-block-a
+{
+	width:70%;
+	font-weight:normal;
 }
-#profile div.ui-block-a span {
-	font-size: 20px;
-	min-width: 50px;
+
+#profile div.ui-block-a span
+{
+	font-size:20px;
+	min-width:50px;
 }
-#profile div.ui-block-a * {
-	vertical-align: middle;
+
+#profile div.ui-block-a *
+{
+	vertical-align:middle;
 }
-#message {
-	font-weight: normal;
-	padding-top:0px;
-	padding-bottom:0px;
+
+#message
+{
+	font-weight:normal;
+	padding-top:0;
+	padding-bottom:0;
 }
-#message .user {
-	font-size: 13px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
-	width: 90px;
+
+#message .user
+{
+	font-size:13px;
+	overflow:hidden;
+	text-overflow:ellipsis;
+	white-space:nowrap;
+	width:90px;
 }
-#message .user * {
-	font-size: 13px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
-	text-align: center;
+
+#message .user *
+{
+	font-size:13px;
+	overflow:hidden;
+	text-overflow:ellipsis;
+	white-space:nowrap;
+	text-align:center;
 }
-#message .ui-block-b {
-	width: 75%;
+
+#message .ui-block-b
+{
+	width:75%;
 }
-#message .menu {
-	text-align: right;
+
+#message .menu
+{
+	text-align:right;
 }
-#message textarea {
-	width: 90%;
+
+#message textarea
+{
+	width:90%;
 }
-#page_room .ui-content {
-	padding: 0px;
+
+#page_room .ui-content
+{
+	padding:0;
 }
-#page_room .ui-header {
-	border-color: #EEEEEE;
+
+#page_room .ui-header
+{
+	border-color:#EEEEEE;
 }
-.dura-btn-submit {
-	text-align: center;
-	width: 95%;
+
+.dura-btn-submit
+{
+	text-align:center;
+	width:95%;
 }
-.dura-btn-submit .ui-btn {
-	border-width: 2px;
-	min-width: 200px;
+
+.dura-btn-submit .ui-btn
+{
+	border-width:2px;
+	min-width:200px;
 }
-.dura-btn-submit .ui-btn-inner {
-	border-top-width: 1px;
-	padding-bottom: 0;
-	padding-top: 0;
+
+.dura-btn-submit .ui-btn-inner
+{
+	border-top-width:1px;
+	padding-bottom:0;
+	padding-top:0;
 }
-#talks dt, #talks dd {
-	margin: 0px;
+
+#talks dt,#talks dd
+{
+	margin:0;
 }
-#talks {
+
+#talks
+{
 }
-#talks div.system {
-	margin: 10px 0 10px 20px;
-	letter-spacing: 3px;
-	clear: left;
+
+#talks div.system
+{
+	margin:10px 0 10px 20px;
+	letter-spacing:3px;
+	clear:left;
 }
-dl.talk {
-	clear: both;
-	min-height: 80px;/*margin-top: 10px;*/
-	}
-dl.talk dt {
-	min-height: 20px;
-	width: 90px;
-	padding-top: 58px;
-	padding-bottom: 10px;
-	text-align: center;
-	display: block;
-	line-height: 20px;
-	float: left;
-	background: transparent url('css/icon/setton.png') no-repeat center top;
+
+dl.talk
+{
+	clear:both;
+	min-height:80px;
+/*margin-top: 10px;*/
 }
-dl.talk dt.select {
-	border: 2px solid #ff6600;
+
+dl.talk dt
+{
+	min-height:20px;
+	width:90px;
+	padding-top:58px;
+	padding-bottom:10px;
+	text-align:center;
+	display:block;
+	line-height:20px;
+	float:left;
+	background:transparent url('css/icon/setton.png') no-repeat center top;
 }
-dl.talk dd {
-	display: inline-block;
-	/*
+
+dl.talk dt.select
+{
+	border:2px solid #ff6600;
+}
+
+dl.talk dd
+{
+	display:inline-block;
+/*
 	max-width: 510px;
 	*/
-	margin-left: 0;
-	overflow: hidden;
+	margin-left:0;
+	overflow:hidden;
 }
-dl.talk dd div.bubble p.body {
-	float: left;
-	clear: left;
-	padding: 15px 20px;
-	border-radius: 13px;
-	border: 4px #fff solid;
-	-moz-border-radius: 13px;
-	-webkit-border-radius: 13px;
-	background: transparent url('css/balloon/gray.png') repeat-x left center;
-	font: 1em "lucida grande", "Meiryo", tahoma, verdana, arial, sans-serif;
-	letter-spacing: 3px;
-	color: white;
-	-position: relative;
-	margin-top: 0px;
 
-	word-break: break-all;
-	word-wrap: break-word;
-	white-space: normal;
-
-	/*
+dl.talk dd div.bubble p.body
+{
+	float:left;
+	clear:left;
+	padding:15px 20px;
+	border-radius:13px;
+	border:4px #fff solid;
+	-moz-border-radius:13px;
+	-webkit-border-radius:13px;
+	background:transparent url('css/balloon/gray.png') repeat-x left center;
+	font:1em "lucida grande", "Meiryo", tahoma, verdana, arial, sans-serif;
+	letter-spacing:3px;
+	color:white;
+	-position:relative;
+	margin-top:0;
+	word-break:break-all;
+	word-wrap:break-word;
+	white-space:normal;
+/*
 	overflow: visible;
 	*/
-
-	/*
+/*
 	width: 100%;
 	*/
-	width: auto;
-	max-width: 510px;
-
-	min-height: 1.5em;
+	width:auto;
+	max-width:510px;
+	min-height:1.5em;
 }
+
 /* !begin icon styles */
-			#talks .tanaka {
-	background-image: url('css/icon/tanaka.png');
+#talks .tanaka
+{
+	background-image:url('css/icon/tanaka.png');
 }
-#talks .kanra {
-	background-image: url('css/icon/kanra.png');
+
+#talks .kanra
+{
+	background-image:url('css/icon/kanra.png');
 }
-#talks .setton {
-	background-image: url('css/icon/setton.png');
+
+#talks .setton
+{
+	background-image:url('css/icon/setton.png');
 }
-#talks .saika {
-	background-image: url('css/icon/saika.png');
+
+#talks .saika
+{
+	background-image:url('css/icon/saika.png');
 }
-#talks .bakyura {
-	background-image: url('css/icon/bakyura.png');
+
+#talks .bakyura
+{
+	background-image:url('css/icon/bakyura.png');
 }
-#talks .gg {
-	background-image: url('css/icon/gg.png');
+
+#talks .gg
+{
+	background-image:url('css/icon/gg.png');
 }
-#talks .zawa {
-	background-image: url('css/icon/zawa.png');
+
+#talks .zawa
+{
+	background-image:url('css/icon/zawa.png');
 }
-#talks .simon {
-	background-image: url('css/icon/simon.png');
+
+#talks .simon
+{
+	background-image:url('css/icon/simon.png');
 }
-#talks .koukin {
-	background-image: url('css/icon/koukin.png');
+
+#talks .koukin
+{
+	background-image:url('css/icon/koukin.png');
 }
-#talks .shinra {
-	background-image: url('css/icon/shinra.png');
+
+#talks .shinra
+{
+	background-image:url('css/icon/shinra.png');
 }
-#talks .sizuo {
-	background-image: url('css/icon/sizuo.png');
+
+#talks .sizuo
+{
+	background-image:url('css/icon/sizuo.png');
 }
-#talks .izaya {
-	background-image: url('css/icon/izaya.png');
+
+#talks .izaya
+{
+	background-image:url('css/icon/izaya.png');
 }
-#talks .chinese {
-	background-image: url('css/icon/chinese.png');
+
+#talks .chinese
+{
+	background-image:url('css/icon/chinese.png');
 }
-#talks .lady {
-	background-image: url('css/icon/lady.png');
+
+#talks .lady
+{
+	background-image:url('css/icon/lady.png');
 }
-#talks .lolita {
-	background-image: url('css/icon/lolita.png');
+
+#talks .lolita
+{
+	background-image:url('css/icon/lolita.png');
 }
-#talks .woman {
-	background-image: url('css/icon/woman.png');
+
+#talks .woman
+{
+	background-image:url('css/icon/woman.png');
 }
-#talks ._admin {
-	background-image: url('css/icon/_admin.png');
+
+#talks ._admin
+{
+	background-image:url('css/icon/_admin.png');
 }
+
 /* !end icon styles */
+/* !begin balloon styles */
+#talks .blue
+{
+	background-image:url('css/balloon/blue.png');
+}
 
-			/* !begin balloon styles */
-			#talks .blue {
-	background-image: url('css/balloon/blue.png');
+#talks .gray
+{
+	background-image:url('css/balloon/gray.png');
 }
-#talks .gray {
-	background-image: url('css/balloon/gray.png');
-}
-#talks .green {
-	background-image: url('css/balloon/green.png');
-}
-#talks .orange {
-	background-image: url('css/balloon/orange.png');
-}
-#talks .pink {
-	background-image: url('css/balloon/pink.png');
-}
-#talks .red {
-	background-image: url('css/balloon/red.png');
-}
-#talks .purple {
-	background-image: url('css/balloon/purple.png');
-}
-#talks .brown {
-	background-image: url('css/balloon/brown.png');
-}
-#talks .cobalt {
-	background-image: url('css/balloon/cobalt.png');
-}
-#talks .strawberry {
-	background-image: url('css/balloon/strawberry.png');
-}
-#talks .yellow {
-	background-image: url('css/balloon/yellow.png');
-}
-#talks .carrot {
-	background-image: url('css/balloon/carrot.png');
-}
-			/* !begin balloon styles */
 
-.avatar .avatar_icon, .avatar .avatar_icon * {
-	display: none;
-	visibility: hidden;
-	padding: 0px;
-	margin: 0px;
+#talks .green
+{
+	background-image:url('css/balloon/green.png');
+}
+
+#talks .orange
+{
+	background-image:url('css/balloon/orange.png');
+}
+
+#talks .pink
+{
+	background-image:url('css/balloon/pink.png');
+}
+
+#talks .red
+{
+	background-image:url('css/balloon/red.png');
+}
+
+#talks .purple
+{
+	background-image:url('css/balloon/purple.png');
+}
+
+#talks .brown
+{
+	background-image:url('css/balloon/brown.png');
+}
+
+#talks .cobalt
+{
+	background-image:url('css/balloon/cobalt.png');
+}
+
+#talks .strawberry
+{
+	background-image:url('css/balloon/strawberry.png');
+}
+
+#talks .yellow
+{
+	background-image:url('css/balloon/yellow.png');
+}
+
+#talks .carrot
+{
+	background-image:url('css/balloon/carrot.png');
+}
+
+/* !begin balloon styles */
+.avatar .avatar_icon,.avatar .avatar_icon *
+{
+	display:none;
+	visibility:hidden;
+	padding:0;
+	margin:0;
 }
