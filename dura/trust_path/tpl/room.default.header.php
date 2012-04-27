8187a515f0594902189062f9b543956cd06df7da
bluelovers <codelovers@users.sourceforge.net>
2012-04-27 16:10:38 +0800

    .

--- dura/trust_path/tpl/room.default.header.php
+++ dura/trust_path/tpl/room.default.header.php
@@ -2,7 +2,7 @@
 	<script type="text/javascript" src="<?php e(DURA_URL) ?>/js/SoundManager2/script/soundmanager2-nodebug-jsmin.js"></script>
 	<script type="text/javascript">
 
-	(function($, soundManager){
+	(function($, soundManager, undefined){
 
 		var messageSound;
 
@@ -18,58 +18,185 @@
 		soundManager.defaultOptions.volume = 100;
 		soundManager.defaultOptions.autoLoad = true;
 
-		soundManager.onready(function() {
-			$.log(['soundManager', 'onready']);
+		$.Dura = $.extend({}, $.Dura, {});
+		$.Dura.sound = $.extend({}, $.Dura.sound, {});
 
-			messageSound = soundManager.createSound({
-				id: 'messageSound',
-				url: '<?php e(DURA_URL) ?>/js/sound.mp3',
-				volume: 100,
-				autoLoad: true,
+		var sound =
+		{
+			data : {
+				is_create: false,
+				_sound: null,
+				_class: soundManager,
+			},
 
-				onload: function() {
-					$.log(['messageSound:loaded -> onload', messageSound.loaded]);
-
-					this.play();
-				},
-			});
-		});
-
-		$.extend($, {
-			sound: function()
+			log : function(data)
 			{
-				if (!messageSound)
-				{
-					messageSound = soundManager.getSoundById('messageSound');
-				}
+				$.extend(data, {}, { sound: $.Dura.sound });
 
-				if (!messageSound)
+				$.log(data);
+
+				return this;
+			},
+
+			create : function()
+			{
+				if (!$.Dura.sound.data.do_create)
 				{
-					messageSound = soundManager.createSound({
+					$.Dura.sound.data.do_create = true;
+
+					$.Dura.sound.log(['Dura.sound -> create']);
+
+					$.Dura.sound.data._sound = soundManager.createSound({
 						id: 'messageSound',
 						url: '<?php e(DURA_URL) ?>/js/sound.mp3',
 						volume: 100,
 						autoLoad: true,
 
-						onload: function() {
-							$.log(['messageSound:loaded -> onload', messageSound.loaded]);
-
+						onload: function(){
+							$.Dura.sound._load();
 						},
 					});
 				}
 
-				$.log(['messageSound:loaded', messageSound.loaded]);
+				return this;
+			},
 
-				if (messageSound && !messageSound.loaded)
+			onready : function(fn)
+			{
+				$.Dura.sound.log(['Dura.sound -> onready']);
+
+				if (fn != undefined) $.Dura.sound.data.onready = fn;
+
+				return this;
+			},
+
+			_ready : function()
+			{
+				if (!$.Dura.sound.data.do_ready)
 				{
-					//soundManager.load('messageSound');
-					messageSound.load();
+					$.Dura.sound.data.do_ready = true;
+
+					$.Dura.sound.log(['Dura.sound -> _ready']);
+
+					if ($.Dura.sound.data.onready)
+					{
+						$.Dura.sound.data.onready();
+					}
+
+					$.Dura.sound.create();
+				}
+			},
+
+			_play : function()
+			{
+				try
+				{
+					soundManager.getSoundById('messageSound').play();
+					$.Dura.sound.log(['Dura.sound -> _play -> ok']);
+				}
+				catch(e)
+				{
+					$.Dura.sound.log(['Dura.sound -> _play -> error']);
+				}
+			},
+
+			play : function()
+			{
+				$.Dura.sound.log(['Dura.sound -> play']);
+
+				if (!$.Dura.sound.data.do_ready)
+				{
+					$.Dura.sound.log(['Dura.sound -> play -> not ready']);
+
+					$.Dura.sound.data.play_on_load = true;
+
+					$.Dura.sound._play();
+
+					return;
 				}
 
+				if ($.Dura.sound.data.do_loaded || $.Dura.sound.data.is_loaded)
+				{
+					$.Dura.sound.log(['Dura.sound -> play -> play']);
+
+					$.Dura.sound._play();
+				}
+				else
+				{
+					$.Dura.sound.log(['Dura.sound -> play -> load']);
+
+					$.Dura.sound.data.play_on_load = true;
+
+					$.Dura.sound
+						.create()
+						.load()
+					;
+				}
+
+				return this;
 			},
+
+			onload : function(fn)
+			{
+				$.Dura.sound.log(['Dura.sound -> onload']);
+
+				if (fn != undefined) $.Dura.sound.data.onload = fn;
+
+				return this;
+			},
+
+			_load : function()
+			{
+				$.Dura.sound.log(['Dura.sound -> _load']);
+
+				if (!$.Dura.sound.data.is_loaded) $.Dura.sound.data.is_loaded = $.Dura.sound.create().loaded;
+
+				if ($.Dura.sound.data.onload)
+				{
+					$.Dura.sound.data.onload();
+				}
+
+				if ($.Dura.sound.data.play_on_load)
+				{
+					$.Dura.sound.log(['Dura.sound -> _load -> play_on_load']);
+
+					$.Dura.sound._play();
+				}
+			},
+
+			load : function()
+			{
+
+				if (!$.Dura.sound.data.do_ready)
+				{
+					$.Dura.sound.log(['Dura.sound -> load -> not ready']);
+
+					return;
+				}
+
+				if (!$.Dura.sound.data.do_loaded)
+				{
+					$.Dura.sound.log(['Dura.sound -> load']);
+					$.Dura.sound.data.do_loaded = true;
+
+					if (!$.Dura.sound.data.is_loaded)
+					{
+						$.Dura.sound.create();
+					}
+				}
+
+				return this;
+			},
+		};
+
+		$.extend($.Dura.sound, {}, sound);
+
+		soundManager.onready(function(){
+			$.Dura.sound.log(['soundManager -> onready']);
+			$.Dura.sound._ready();
 		});
 
-		$.sound();
+		//$.sound();
 
 	})(jQuery, soundManager);
 
