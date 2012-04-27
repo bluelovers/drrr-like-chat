d5eff23fe51f3bbde9efd8271ccb03d1770e082e
bluelovers <codelovers@users.sourceforge.net>
2012-04-27 21:08:47 +0800

    .

--- dura/trust_path/dura.php
+++ dura/trust_path/dura.php
@@ -419,28 +419,3 @@ class Dura
 	}
 	// bluelovers
 }
-
-function t($message)
-{
-	if (isset(Dura::$catalog[$message]))
-	{
-		$message = Dura::$catalog[$message];
-	}
-
-	if (func_num_args() == 1) return $message;
-
-	$params = func_get_args();
-
-	foreach ($params as $i => $param)
-	{
-		$message = str_replace('{' . $i . '}', $param, $message);
-	}
-
-	return $message;
-}
-
-function e($string, $escape = false)
-{
-	echo $escape ? Dura::escapeHtml($string) : $string;
-}
-
