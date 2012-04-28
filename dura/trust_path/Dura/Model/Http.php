141d41336115fd3864c5e7c60835ece562d86a32
bluelovers <codelovers@users.sourceforge.net>
2012-04-28 13:48:06 +0800

    .

--- dura/trust_path/Dura/Model/Http.php
+++ dura/trust_path/Dura/Model/Http.php
@@ -8,6 +8,14 @@
 class Dura_Model_Http
 {
 
+	public static $map = array(
+		'contenttype' => array(
+			'json' => 'application/json',
+			'xml' => 'application/xml',
+			'html' => 'text/html',
+		),
+	);
+
 	static function header($string, $replace = true, $http_response_code = null)
 	{
 		return @header($string, $replace, $http_response_code);
@@ -16,6 +24,11 @@ class Dura_Model_Http
 	static function expires($seconds = 60, $last_modified = 0, $now = 0)
 	{
 		return Dura_Model_Http_Expires::check($seconds, $last_modified, $now);
+	}
+
+	function getContentType($type = 'html')
+	{
+		return isset(self::$map['contenttype'][$type]) ? self::$map['contenttype'][$type] : self::$map['contenttype']['html'];
 	}
 
 }
