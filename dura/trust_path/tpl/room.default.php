f53d142597044299dc6eb42027a2d28dd9126074
bluelovers <codelovers@users.sourceforge.net>
2012-04-29 21:58:24 +0800

    split

--- dura/trust_path/tpl/room.default.php
+++ dura/trust_path/tpl/room.default.php
@@ -86,25 +86,7 @@
 
 			<div class="main-box content-primary" id="talks">
 
-				<?php foreach ( $dura['room']['talks'] as $talk ) : ?>
-					<?php if ( !$talk['uid'] ) : ?>
-						<div class="talk system" id="<?php e($talk['id']) ?>" <?php e($talk['time'] <= $dura['input']['last_talk_time'] ? ' dura-show="1"' : ''); ?> >
-							<?php e($talk['message']) ?>
-						</div>
-					<?php else: ?>
-						<dl class="talk icon_<?php e($talk['icon']) ?> <?php if ( $talk['uid'] == $dura['room']['host'] ) :?> dura-ishost <?php endif; ?>" id="<?php e($talk['id']) ?>" <?php e($talk['time'] <= $dura['input']['last_talk_time'] ? ' dura-show="1"' : ''); ?> >
-							<dt class="avatar <?php e($talk['icon']) ?>" title="<?php e($talk['name']) ?>">
-								<div class="avatar_icon"><img src="<?php e(Dura_Class_Icon::getIconUrl($dura['user']['icon'])) ?>" title="<?php e($talk['name']) ?>"></div>
-								<div class="name"><?php e($talk['name']) ?></div>
-							</dt>
-							<dd>
-								<div class="bubble">
-									<div class="body <?php e($talk['color']) ?>"><?php e($talk['message']) ?></div>
-								</div>
-							</dd>
-						</dl>
-					<?php endif ?>
-				<?php endforeach ?>
+				<?php e($this->slot('room.default.talks.talk'));?>
 
 			</div>
 
