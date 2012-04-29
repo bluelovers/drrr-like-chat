c1b86f52b304715c336afa5cf6d2e14aa466cd5f
bluelovers <codelovers@users.sourceforge.net>
2012-04-29 21:50:13 +0800

    .

--- dura/trust_path/Dura/Model/Http.php
+++ dura/trust_path/Dura/Model/Http.php
@@ -18,12 +18,13 @@ class Dura_Model_Http
 
 	static function header($string, $replace = true, $http_response_code = null)
 	{
-		return @header($string, $replace, $http_response_code);
-	}
+		if (0)
+		{
+			echo $string.'<br/>';
+			return;
+		}
 
-	static function expires($seconds = 60, $last_modified = 0, $now = 0)
-	{
-		return Dura_Model_Http_Expires::check($seconds, $last_modified, $now);
+		return @header($string, $replace, $http_response_code);
 	}
 
 	function getContentType($type = 'html')
