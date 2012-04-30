f47148f506780ecadf802e2ec4fdbd9024c78ce3
bluelovers <codelovers@users.sourceforge.net>
2012-04-30 16:06:09 +0800

    <a href="http://www.facebook.com/sharer.php?u=<?php e(rawurlencode($this->get('tpl.header.canonical', DURA_URL))); ?>" target="_blank">

--- dura/trust_path/tpl/theme.content.secondary.php
+++ dura/trust_path/tpl/theme.content.secondary.php
@@ -1,7 +1,9 @@
 
 			<div class="content-secondary" data-theme="a" data-role="fieldcontain">
 				<p>
-					<img src="http://chart.apis.google.com/chart?cht=qr&chs=120x120&chl=<?php e(rawurlencode($this->get('tpl.header.canonical', DURA_URL))); ?>&chld=H|0" alt="<?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?>"/>
+					<a href="http://www.facebook.com/sharer.php?u=<?php e(rawurlencode($this->get('tpl.header.canonical', DURA_URL))); ?>" target="_blank">
+						<img src="http://chart.apis.google.com/chart?cht=qr&chs=120x120&chl=<?php e(rawurlencode($this->get('tpl.header.canonical', DURA_URL))); ?>&chld=H|0" alt="<?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?>"/>
+					</a>
 				</p>
 			</div>
 
