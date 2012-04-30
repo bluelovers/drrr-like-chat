a3aad25bf52292284f0d2b89ef8189c6d3774fde
bluelovers <codelovers@users.sourceforge.net>
2012-04-30 14:20:24 +0800

    <base href="<?php e(DURA_URL); ?>" target="_blank" />

--- dura/trust_path/tpl/theme.php
+++ dura/trust_path/tpl/theme.php
@@ -12,6 +12,8 @@
 
 	<title><?php e($this->get('html.title', t(DURA_TITLE).' | '.t(DURA_SUBTITLE))) ?></title>
 
+	<base href="<?php e(DURA_URL); ?>" target="_blank" />
+
 	<link rel="stylesheet" href="<?php e(DURA_URL) ?>/_temp/jquery-mobile/themes/dura.min.css" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile.structure-1.1.0.min.css" />
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
