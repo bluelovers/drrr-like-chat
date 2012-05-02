d2081b5f905fd9942166785ce5e81bb092d8ca60
bluelovers <codelovers@users.sourceforge.net>
2012-05-03 02:11:33 +0800

    $_icon_color = new Dura_Resource_Colors(); (Step: 1)
    git-Lazy Commit Each Change Script
    
    Output:
    
    Step: 1
    
    [A ] new file:
    
    	trust_path/Dura/Resource/Colors.php

---
+++ dura/trust_path/Dura/Resource/Colors.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class Dura_Resource_Colors extends Dura_Class_Array
+{
+	protected $_data_default_ = array(
+		'lolita' => 'strawberry',
+		'sizuo' => 'gray',
+		'lady' => 'carrot',
+		'koukin' => 'yellow',
+		'bakyura' => 'green',
+		'tanaka' => 'blue',
+		'gg' => 'pink',
+		'admin' => 'orange',
+		'zaika' => 'red',
+		'saika' => 'red',
+		'zawa' => 'green',
+		'kanra' => 'orange',
+		'setton' => 'gray',
+		'woman' => 'carrot',
+		'izaya' => 'brown',
+		'chinese' => 'purple',
+		'simon' => 'cobalt',
+		);
+}
