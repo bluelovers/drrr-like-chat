c0ecb8954d6b12ca731436f23763d68d5bcfaff8
bluelovers <codelovers@users.sourceforge.net>
2012-05-02 23:08:20 +0800

    {js => static/js} (Step: 5)
    git-Lazy Commit Each Change Script
    
    Output:
    
    Step: 5
    
    [M ] modified:
    
    	trust_path/tpl/room.default.header.php

--- dura/trust_path/tpl/room.default.header.php
+++ dura/trust_path/tpl/room.default.header.php
@@ -5,10 +5,10 @@ GlobalMessageMaxLength = <?php e(DURA_MESSAGE_MAX_LENGTH) ?>;
 useComet = <?php e(DURA_USE_COMET) ?>;
 </script>
 
-	<script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.corner.js"></script>
-	<!--script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.chat.js"></script-->
+	<script type="text/javascript" src="<?php e(DURA_URL) ?>/static/js/jquery.corner.js"></script>
+	<!--script type="text/javascript" src="<?php e(DURA_URL) ?>/static/js/jquery.chat.js"></script-->
 
-	<script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="<?php e(DURA_URL) ?>/static/js/jquery.easing.1.3.js"></script>
 
 <?php e($this->slot('room.default.js.sound'));?>
 
